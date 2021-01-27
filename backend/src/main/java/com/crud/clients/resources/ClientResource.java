7b9511f58ab4a3fd2e6c9847c91ccb49449182f0
rensilver <ren.oliv87@gmail.com>
2021-01-26 23:09:14 -0300

    DTO

--- backend/src/main/java/com/crud/clients/resources/ClientResource.java
+++ backend/src/main/java/com/crud/clients/resources/ClientResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.crud.clients.dto.ClientDTO;
 import com.crud.clients.entities.Client;
 import com.crud.clients.services.ClientService;
 
@@ -19,8 +20,8 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/crud/clients/services/ClientService.java
+++ backend/src/main/java/com/crud/clients/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.crud.clients.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.crud.clients.dto.ClientDTO;
 import com.crud.clients.entities.Client;
 import com.crud.clients.repositories.ClientRepository;
 
@@ -16,7 +18,8 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 }
