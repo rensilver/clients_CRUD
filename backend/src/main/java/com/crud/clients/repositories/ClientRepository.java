7c5350c65e36ced874906f199ed5fa366d17f7d1
rensilver <ren.oliv87@gmail.com>
2021-01-29 23:42:14 -0300

    POST new Client

--- backend/src/main/java/com/crud/clients/repositories/ClientRepository.java
+++ backend/src/main/java/com/crud/clients/repositories/ClientRepository.java
@@ -1,9 +1,11 @@
 package com.crud.clients.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.crud.clients.entities.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
  
 }
