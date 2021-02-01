4ea664101dd319eb1576b55ba74e6766873e7939
rensilver <ren.oliv87@gmail.com>
2021-01-31 21:04:38 -0300

    Delete client

--- backend/src/main/java/com/crud/clients/services/ClientService.java
+++ backend/src/main/java/com/crud/clients/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.crud.clients.dto.ClientDTO;
 import com.crud.clients.entities.Client;
 import com.crud.clients.repositories.ClientRepository;
+import com.crud.clients.services.exceptions.DatabaseException;
 import com.crud.clients.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -54,6 +57,19 @@ public class ClientService {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
 	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
+	}
 
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 
@@ -63,5 +79,4 @@ public class ClientService {
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
 	}
-
 }
