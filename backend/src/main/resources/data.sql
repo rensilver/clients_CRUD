475ae37698ec2914e02728f69d74e2ce8e5efbd0
rensilver <ren.oliv87@gmail.com>
2021-01-31 22:58:29 -0300

    Database Seeding updated

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,10 @@
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Antonio João', '22456879004', 6500.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 2);
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marina Moura', '98765432102', 4500.0, TIMESTAMP WITH TIME ZONE '2020-07-15T10:00:00Z', 1);
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Beto Paulo', '45678912303', 2500.0, TIMESTAMP WITH TIME ZONE '2020-07-10T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Mauro Vitor', '54685263904', 7500.0, TIMESTAMP WITH TIME ZONE '2020-07-18T10:00:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Francisco Eduardo', '22312458703', 3000.0, TIMESTAMP WITH TIME ZONE '2020-07-20T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Maria', '12356487902', 8400.0, TIMESTAMP WITH TIME ZONE '2020-08-10T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Juliana Gonzaga', '11255034603', 2000.0, TIMESTAMP WITH TIME ZONE '2020-06-28T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Antonio', '89755732101', 1600.0, TIMESTAMP WITH TIME ZONE '2020-07-09T10:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lourdes Maria', '87863621404', 4200.0, TIMESTAMP WITH TIME ZONE '2020-07-12T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Alex Gomes', '54289723102', 3500.0, TIMESTAMP WITH TIME ZONE '2020-10-11T10:00:00Z', 0);
